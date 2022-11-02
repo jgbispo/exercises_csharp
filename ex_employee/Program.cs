0f07f761c21b01f2d81307f51ef0d76285da17ba
bshop <jgbispo20@gmail.com>
2022-11-02 04:18:26 -0300

    Employee Exercise - Done

--- ex_employee/Program.cs
+++ ex_employee/Program.cs
@@ -6,8 +6,15 @@ namespace EmployeeTax
     {
         static void Main(string[] args)
         {
-            Employee employee = new Employee("John", 2000.00, 500.00);
-            Console.WriteLine(employee);
+            Console.Write("Take the name of the employee: ");
+            string name = Console.ReadLine()!;
+            Console.Write("Take the salary of the employee: ");
+            double salary = double.Parse(Console.ReadLine()!);
+            Console.Write("Take the tax of the employee: ");
+            double tax = double.Parse(Console.ReadLine()!);
+            Employee employee = new Employee(name, salary, tax);
+            Console.WriteLine();
+            Console.WriteLine("Employee: " + employee);
             Console.Write("Enter the percentage to increase salary: ");
             double percentage = double.Parse(Console.ReadLine()!);
             employee.IncreaseSalary(percentage);
