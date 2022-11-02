60df1ee1e680177e96d5a997741395e41dac4a00
bshop <jgbispo20@gmail.com>
2022-11-02 10:44:48 -0300

    Controll Time

--- ex_parking/ParkingControl.cs
+++ ex_parking/ParkingControl.cs
@@ -23,6 +23,77 @@ namespace ParkingLot
       }
     }
 
+    public void removeVacancyByBoard(string board)
+    {
+      for (int i = 0; i < this.vacancies.Length; i++)
+      {
+        if (this.vacancies[i] != null)
+        {
+          if (this.vacancies[i].Automobiles.Board == board)
+          {
+            this.vacancies[i].ExitTime = DateTime.Now;
+            ShowVacanciesByBoard(board);
+            CalcTimeByBoard(board);
+            AmountPayable(board);
+            this.vacancies[i] = null;
+            break;
+          }
+        }
+      }
+    }
+
+    public void CalcTimeByBoard(string board)
+    {
+      for (int i = 0; i < this.vacancies.Length; i++)
+      {
+        if (this.vacancies[i] != null)
+        {
+          if (this.vacancies[i].Automobiles.Board == board)
+          {
+            TimeSpan time = this.vacancies[i].ExitTime - this.vacancies[i].EntryTime;
+            Console.WriteLine("Time: " + time);
+            break;
+          }
+        }
+      }
+    }
+
+    public void AmountPayable(string board)
+    {
+      for (int i = 0; i < this.vacancies.Length; i++)
+      {
+        if (this.vacancies[i] != null)
+        {
+          if (this.vacancies[i].Automobiles.Board == board)
+          {
+            TimeSpan time = this.vacancies[i].ExitTime - this.vacancies[i].EntryTime;
+            double amount = time.TotalHours * 5;
+            Console.WriteLine("Amount payable: " + amount);
+          }
+        }
+      }
+    }
+
+    public void ShowVacanciesByBoard(string board)
+    {
+      for (int i = 0; i < this.vacancies.Length; i++)
+      {
+        if (this.vacancies[i] != null)
+        {
+          if (this.vacancies[i].Automobiles.Board == board)
+          {
+            Console.WriteLine("Board: " + this.vacancies[i].Automobiles.Board);
+            Console.WriteLine("Color: " + this.vacancies[i].Automobiles.Color);
+            Console.WriteLine("Name: " + this.vacancies[i].Persons.Name);
+            Console.WriteLine("CPF: " + this.vacancies[i].Persons.Document);
+            Console.WriteLine("Entry time: " + this.vacancies[i].EntryTime);
+            Console.WriteLine("Exit time: " + this.vacancies[i].ExitTime);
+            break;
+          }
+        }
+      }
+    }
+
     public void ShowVacanciesEmpty()
     {
       for (int i = 0; i < this.vacancies.Length; i++)
@@ -44,6 +115,7 @@ namespace ParkingLot
           Console.WriteLine("Person: " + this.vacancies[i].Persons.Name);
           Console.WriteLine("Automobile: " + this.vacancies[i].Automobiles.Board);
           Console.WriteLine("Color: " + this.vacancies[i].Automobiles.Color);
+          Console.WriteLine("Entry Time: " + this.vacancies[i].EntryTime);
           Console.WriteLine();
         }
       }
